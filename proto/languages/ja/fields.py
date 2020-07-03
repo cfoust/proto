89e9c7121db8808d9900a73870c814d3d3172c25
Caleb Foust <cfoust@sqweebloid.com>
2020-07-03 08:37:10 -0700

    Remove old Japanese files

--- proto/languages/ja/fields.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""File that describes all of the fields necessary for Japanese's cards."""
-
-from ...fields import FieldType
-from ...butils import get_file_lines
-from jmdictparse import *
-
-import os, json
