fcd319a3c1dc87e0f36baeca1be6b842c7738b81
Caleb Foust <cfoust@sqweebloid.com>
2020-07-29 12:05:48 -0700

    Completely redo caching mechanism, remove peewee

--- proto/caching.py
+++ proto/caching.py
@@ -1,52 +1,69 @@
+"""
+Contains generalized caching logic for all of proto's datatypes.
+"""
+
+import sqlite3
 import datetime
 import zlib
-import peewee
+import json
 
-from typing import (
-    Union, Optional, Tuple, List, Callable
-)
+from typing import Union, Optional, Tuple, List, Callable
 from proto.field import FieldResult
 
-# Standin proxy for a potential peewee database
-dbproxy = peewee.Proxy()
+RowType = Tuple[
+    # type
+    str,
+    # timestamp
+    datetime.datetime,
+    # key
+    str,
+    # is_media
+    bool,
+    # media_json
+    Optional[str],
+    # data
+    Optional[bytes],
+]
+
+PackType = Tuple[
+    bool, Optional[str], Optional[bytes],
+]
 
 
-class CachedInfo(peewee.Model):
-    """This is the model that we use so peewee can store cached information
-       in the database. We automatically compress when above a certain size
-       threshold."""
-
-    db_name = peewee.CharField()
-    timestamp = peewee.DateTimeField()
-    key = peewee.TextField()
-    data = peewee.BlobField(null=True)
-    is_string = peewee.BooleanField()
-
-    class Meta:
-        database = dbproxy
-
-
-CacheData = Union[str, bytes, None]
-
-
-def pack_data(data: CacheData) -> Optional[bytes]:
-    if not data:
-        return None
+def pack_data(data: FieldResult) -> PackType:
+    if data is None:
+        return (False, None, None)
 
     if isinstance(data, str):
-        data = data.encode("utf-8")
+        return (False, None, data.encode("utf-8"))
 
-    return zlib.compress(data)
+    # Has to be media
+    return (
+        True,
+        json.dumps({"filename": data["filename"], "field": data["field"],}),
+        zlib.compress(data["data"]),
+    )
 
 
-def unpack_data(data: Optional[bytes], is_string: bool) -> CacheData:
-    if not data:
+def unpack_data(data: PackType) -> FieldResult:
+    is_media, media_json, blob = data
+
+    if is_media:
+        # Something is off
+        if media_json is None or blob is None:
+            return None
+
+        dejson = json.loads(media_json)
+        return {
+            "filename": dejson["filename"],
+            "field": dejson["field"],
+            "data": zlib.decompress(blob),
+        }
+
+    if blob is None:
         return None
 
-    blob = zlib.decompress(data)
-    if not is_string:
-        return blob
-
+    # Otherwise it's a string
     return blob.decode("utf-8")
 
 
@@ -62,124 +79,162 @@ class Cache:
         # The 'namespace' for all entries stored using this class instance.
         identifier: str,
     ):
-        """Makes a connection to the provided peewee db."""
-        db = peewee.SqliteDatabase(db_path)
-        db.connect(reuse_if_open=True)
-        dbproxy.initialize(db)
+        db = sqlite3.connect(db_path, detect_types=sqlite3.PARSE_DECLTYPES)
+
+        db.execute(
+            """
+CREATE TABLE IF NOT EXISTS proto_cache (
+    -- The "namespace" of the row
+    type TEXT NOT NULL,
+    -- When the row was last updated
+    timestamp TIMESTAMP NOT NULL,
+    -- The primary key of the individual row
+    key TEXT NOT NULL,
+    -- Whether the blob is a media blob
+    is_media BOOLEAN NOT NULL,
+    -- This contains the Media dict if is_media is True
+    media_json TEXT,
+    -- If is_media, contains the media file, otherwise this is the string.
+    data BLOB
+)
+        """
+        )
+        db.commit()
 
         self.db = db
-
-        # Create the tables if they don't exist
-        self.db.create_tables([CachedInfo], safe=True)
 
         """The identifier is a unique string that all data cached by this
            instance uses to differentiate from other caches in the database."""
         self.identifier = identifier
 
-    def _get_row(self, key: str):
-        return CachedInfo.get(
-            CachedInfo.db_name == self.identifier, CachedInfo.key == key
+    def _get_row(self, key: str) -> Optional[RowType]:
+        results = self.db.execute(
+            """
+SELECT * from proto_cache WHERE type = ? AND key = ?
+""",
+            (self.identifier, key),
         )
+
+        return results.fetchone()
 
     def exists(self, key: str) -> bool:
         """
         Checks whether some data exists for a key.
         """
+        return self._get_row(key) is not None
 
-        try:
-            info = self._get_row(key)
-            return True
-        except peewee.DoesNotExist:
-            return False
-
-    def retrieve(self, key: str) -> Optional[Tuple[datetime.datetime, CacheData]]:
+    def retrieve(self, key: str) -> Optional[Tuple[datetime.datetime, FieldResult]]:
         """
         Pulls some cached info from the DB.
 
         Returns a tuple of (timestamp, data) if the word exists and None if
         it does not.
         """
-        try:
-            # Grab the word from the database
-            info = self._get_row(key)
-            value = unpack_data(info.data, info.is_string)
+        # Grab the word from the database
+        row = self._get_row(key)
 
-            # Return the data and its timestamp.
-            return (info.timestamp, value)
-        except peewee.DoesNotExist:
+        if not row:
             return None
 
-    def store(self, key: str, data: CacheData) -> None:
+        identifier, ts, key, is_media, media_json, data = row
+
+        value = unpack_data((is_media, media_json, data))
+
+        # Return the data and its timestamp.
+        return (ts, value)
+
+    def _upsert(self, values: List[RowType]):
+        self.db.executemany(
+            """
+INSERT OR REPLACE INTO proto_cache (
+    type,
+    timestamp,
+    key,
+    is_media,
+    media_json,
+    data
+) VALUES (?, ?, ?, ?, ?, ?)
+        """,
+            values,
+        )
+        self.db.commit()
+
+    def store(self, key: str, data: FieldResult) -> None:
         """
         This can create new entries or update old ones.
         """
-        is_string = type(data) is str
-        value = pack_data(data)
 
-        # If data for the key already exists in the database, update it
-        if self.exists(key):
-            info = self._get_row(key)
-            info.is_string = is_string
-            info.data = value
-            info.timestamp = datetime.datetime.now()
-            info.save()
-        # Otherwise create a new entry
-        else:
-            info = CachedInfo.create(
-                db_name=self.identifier,
-                timestamp=datetime.datetime.now(),
-                key=key,
-                data=value,
-                is_string=is_string,
-            )
+        is_media, media_json, blob = pack_data(data)
 
-    def store_many(self, data: List[Tuple[str, CacheData]]) -> None:
+        self._upsert(
+            [
+                (
+                    self.identifier,
+                    datetime.datetime.now(),
+                    key,
+                    is_media,
+                    media_json,
+                    blob,
+                )
+            ]
+        )
+
+    def store_many(self, data: List[Tuple[str, FieldResult]]) -> None:
         """
         Convenience method that imports data en masse. Each entry in the 'data'
         parameter is a tuple such that (input,output).
         """
-        transformed = []
+        transformed: List[RowType] = []
 
         # We create a temporary array to add all our metadata
         for key, datum in data:
-            blob = pack_data(datum)
-            is_string = type(datum) is str
+            is_media, media_json, blob = pack_data(datum)
             transformed.append(
-                {
-                    "is_string": is_string,
-                    "data": blob,
-                    "timestamp": datetime.datetime.now(),
-                    "db_name": self.identifier,
-                    "key": key,
-                }
+                (
+                    self.identifier,
+                    datetime.datetime.now(),
+                    key,
+                    is_media,
+                    media_json,
+                    blob,
+                )
             )
 
-        # db.atomic is much faster for many writes
-        with self.db.atomic():
-            CachedInfo.insert_many(transformed).execute()
+        self._upsert(transformed)
+
 
     def delete(self, key: str) -> None:
         """
         Deletes all data for a given key.
         """
-        info = self._get_row(key)
-        info.delete_instance()
+        self.db.execute(
+            """
+DELETE FROM proto_cache WHERE type = ? AND key = ?
+        """,
+            (self.identifier, key,)
+        )
+        self.db.commit()
+
 
     def clear(self) -> None:
         """
         Wipes all of the rows with this Cache's identifier.
         """
-        CachedInfo.delete().where(CachedInfo.db_name == self.identifier).execute()
+        self.db.execute(
+            """
+DELETE FROM proto_cache WHERE type = ?
+        """,
+            (self.identifier,)
+        )
+        self.db.commit()
+
 
     def rename(self, new_name: str) -> None:
         """
         Renames the identifier used by this Cache. Does not check for
         conflicts on purpose, as we occasionally want to combine datasets.
         """
-        CachedInfo.update(db_name=new_name).where(
-            CachedInfo.db_name == self.identifier
-        ).execute()
-        self.identifier = new_name
+        raise Exception('todo')
 
 
 class TimeCache(object):
@@ -195,7 +250,7 @@ class TimeCache(object):
         self,
         db_path: str,
         identifier: str,
-        generator: Callable[[str], CacheData],
+        generator: Callable[[str], FieldResult],
         # The amount of time we should wait before trying to call
         # the field again after it returns None initially.
         retry: datetime.timedelta = datetime.timedelta(weeks=2),
