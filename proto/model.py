240bfb8b6310f4b9823c42bef6f96d1dcf365a02
Caleb Foust <cfoust@sqweebloid.com>
2020-07-30 08:50:33 -0700

    Kind of get note generation working

--- proto/model.py
+++ proto/model.py
@@ -1,12 +1,25 @@
-from typing import List, Optional, Generic, TypeVar, Callable
+from typing import List, Optional, Generic, TypeVar, Callable, Tuple
+import genanki
 
 from mypy_extensions import TypedDict
 
-from proto.field import Field, FieldResult
+
+from proto.field import Field, FieldResult, Media
 
 Data = TypeVar("Data")
 
 Template = TypedDict("Template", {"name": str, "front": str, "back": str,})
+
+
+def normalize(data: FieldResult) -> Optional[str]:
+    if data is None:
+        return None
+
+    if isinstance(data, str):
+        return data
+
+    # media
+    return data["field"]
 
 
 class Model(Generic[Data]):
@@ -25,8 +38,8 @@ class Model(Generic[Data]):
         self.id = int
         self.name = name
         self.guid = guid
-        self.fields = fields
-        self.templates = templates
+        self.fields = fields or []
+        self.templates = templates or []
 
     def build(self, data: List[Data]) -> Optional[List[List[FieldResult]]]:
         fields = self.fields
@@ -35,4 +48,44 @@ class Model(Generic[Data]):
 
         return list(
             map(lambda row: list(map(lambda field: field.run(row), fields or [])), data)
+        )
+
+    def to_genanki(self, data: List[Data]) -> Tuple[List[genanki.Note], List[Media]]:
+        results = self.build(data)
+
+        if not results:
+            return ([], [])
+
+        # Need to transform all Media to their normal, string format
+        normalized: List[List[Optional[str]]] = []
+        media: List[Media] = []
+        for note in results:
+            fixed_note = list(map(normalize, note))
+            normalized.append(fixed_note)
+
+            # Grab all the media
+            for field in note:
+                if field is None or isinstance(field, str):
+                    continue
+                media.append(field)
+
+        model = genanki.Model(
+            self.id,
+            self.name,
+            fields=list(map(lambda v: {"name": v.name}, self.fields)),
+            templates=list(
+                map(
+                    lambda v: {
+                        "name": v["name"],
+                        "qfmt": v["front"],
+                        "afmt": v["back"],
+                    },
+                    self.templates,
+                )
+            ),
+        )
+
+        return (
+            list(map(lambda v: genanki.Note(model=model, fields=v), normalized)),
+            media,
         )
